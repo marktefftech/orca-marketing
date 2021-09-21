c5e4e6cdc40c90fd971c425f86530364aebc4568
marktefftech <markteffeteller2@gmail.com>
2021-09-21 11:36:41 -0500

    update nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -71,8 +71,8 @@ export class Navigation extends Component {
             <Logo />
           </Link>
           <div className="Nav--Links">
+          <NavLink to="/components/">Components</NavLink>
             <NavLink to="/">Home</NavLink>
-            <NavLink to="/components/">Components</NavLink>
             <div
               className={`Nav--Group ${
                 this.state.activeSubNav === 'posts' ? 'active' : ''
